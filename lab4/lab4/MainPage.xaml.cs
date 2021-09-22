9823668318266212dd8abdba6a722a54eb9f640f
Teseo15 <brayan.blas.egaovil30@gmail.com>
2021-09-22 14:15:01 -0500

    Agregar archivos de proyecto.

---
+++ lab4/lab4/MainPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace lab4
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            item1.Clicked += async (sender, e) =>
+            {
+                await Navigation.PushAsync(new Page1());
+            };
+            item2.Clicked += async (sender, e) =>
+            {
+                await Navigation.PushAsync(new TabbedPage1());
+            };
+            item3.Clicked += async (sender, e) =>
+            {
+                await Navigation.PushAsync(new CarouselPage1());
+            };
+            item5.Clicked += async (sender, e) =>
+            {
+                await Navigation.PushModalAsync(new ModalPage1());
+            };
+            item6.Clicked += async (sender, e) =>
+            {
+                var answer = await DisplayAlert("Question?", "Would you like to play a game", "Yes", "No");
+                Debug.WriteLine("Answer:" + answer);
+            };
+
+
+        }
+    }
+}
